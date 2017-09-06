cf2612f0e351b586c5e494c8a5eb4a37c72b1f97
Guillermo M. Rios <wishvids@gmail.com>
2017-09-05 23:07:09 -0300

    1st

---
+++ src/test/java/celfar/NewTest.java
@@ -0,0 +1,40 @@
+package celfar;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+
+public class NewTest {
+	
+	public WebDriver driver;
+	public String baseUrl;
+	
+  @Test
+  public void f() {
+	  
+	  driver.get(baseUrl);
+	  String t = driver.getTitle();
+	  assertEquals(t, "CelFar");
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  
+	  System.setProperty("webdriver.chrome.driver", "e:\\drivers\\chromedriver.exe");
+	  driver = new ChromeDriver();
+	  baseUrl = "http://nahual.github.io/qc-celfar/?v=1";
+	  
+  }
+
+  @AfterClass
+  public void afterClass() throws InterruptedException {
+	  
+	 Thread.sleep(2000);
+	 driver.close();
+  }
+
+}
---
+++ src/test/java/celfar/NewTest2.java
@@ -0,0 +1,44 @@
+package celfar;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+
+public class NewTest2 {
+	
+	public WebDriver driver;
+	public String baseUrl;
+	
+  @Test
+  public void f() {
+	  
+	  driver.get(baseUrl);
+	  driver.findElement(By.id("input")).sendKeys("10");
+	  driver.findElement(By.className("button")).click();
+	  String out = driver.findElement(By.id("output")).getText();
+	  assertEquals(out, "50");
+	  
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  
+	  System.setProperty("webdriver.chrome.driver", "e:\\drivers\\chromedriver.exe");
+	  driver = new ChromeDriver();
+	  baseUrl = "http://nahual.github.io/qc-celfar/?v=1";
+	  
+  }
+
+  @AfterClass
+  public void afterClass() throws InterruptedException {
+	  
+	 Thread.sleep(2000);
+	 driver.close();
+  }
+
+}
